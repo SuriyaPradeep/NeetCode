472d575ae9dc96380f0f15e442d2e1778510ecbc
Suriya <suriyapradeep2003@gmail.com>
2024-03-18 11:26:43 +0530

    Create ContainsDuplicate.java

---
+++ src/Array_Hashing/ContainsDuplicate.java
@@ -0,0 +1,37 @@
+package Array_Hashing;
+
+import java.util.*;
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ */
+public class ContainsDuplicate {
+    /*
+    Here we will be using an set.Which will store the elements that are already visited.
+    set.add return a boolean value true if the new element is added false if not added.
+    Set will not add elements that are already present in the set.
+    so if set.add() is false then return true.
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer>set=new HashSet<>();
+        for(int num:nums){
+            if(!set.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
