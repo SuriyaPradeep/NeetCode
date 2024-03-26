1e9855a8b208df87d517487c4f2b567c69b0f376
Suriya <suriyapradeep2003@gmail.com>
2024-03-26 21:03:23 +0530

    Create ListNode.java

---
+++ src/Linked_List/ListNode.java
@@ -0,0 +1,15 @@
+package Linked_List;
+//Definition for singly-linked list.
+
+public class ListNode {
+    private int val;
+    private ListNode next;
+    public ListNode(){}
+    public ListNode(int val){
+        this.val=val;
+    }
+    public ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
+    }
+}
